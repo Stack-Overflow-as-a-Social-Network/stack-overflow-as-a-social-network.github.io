e42658637cc09dbd1ce786e361a4623883f269a4
dinotuku <kuan.tung@epfl.ch>
2019-12-15 20:39:36 +0100

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,69 @@
+const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector(".title");
+const text2 = intro.querySelector(".desp");
+const text3 = intro.querySelector("p");
+//END SECTION
+// const section = document.querySelector("section");
+// const end = section.querySelector("h1");
+
+//SCROLLMAGIC
+const controller = new ScrollMagic.Controller();
+
+//Scenes
+let scene = new ScrollMagic.Scene({
+  duration: 9000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setPin(intro)
+  .addTo(controller);
+
+//Text Animation
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+const textAnim1 = TweenMax.fromTo(text2, 3, { opacity: 0 }, { opacity: 1 });
+
+let scene3 = new ScrollMagic.Scene({
+  duration: 3000,
+  offset: 4000,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim1)
+  .addTo(controller);
+
+const textAnim2 = TweenMax.fromTo(text3, 2, { opacity: 0 }, { opacity: 1});
+
+let scene4 = new ScrollMagic.Scene({
+  duration: 2000,
+  offset: 6500,
+  triggerElement: intro,
+  triggerHook: 0
+})
+  .setTween(textAnim2)
+  .addTo(controller);
+
+//Video Animation
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", e => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay += (scrollpos - delay) * accelamount;
+  // console.log(scrollpos, delay);
+
+  video.currentTime = (delay);
+}, 16.67);
